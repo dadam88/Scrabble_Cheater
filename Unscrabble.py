5c3c1d2ac9d83c36e46bb01aa0aafb39be13d223
Derek <derekadam1988@gmail.com>
2015-03-10 18:25:48 -0500

    added scoring mechanic

--- Unscrabble.py
+++ Unscrabble.py
@@ -1,9 +1,15 @@
 import argparse
 
-
 def main(tiles):
+
+	scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+         "x": 8, "z": 10}
+
 	wordlist = []
-	#using with automaticall closes the file when you moveo ut of the code, this is equivalent to f.open, f.close, this is a STANDARD when working with files in PYTHON
+	
 	with open('sowpods.txt', 'r') as f:
 		for line in f:
 			wordlist.append(line.strip())
@@ -11,35 +17,20 @@ def main(tiles):
 	valid_words = []
 	for word in wordlist:
 		rack = tiles
-		# rack = 'ZAEFIEE'
 		used_letters = ''
-		# print word + "\t",
 		for letter in rack.lower():
 			if letter in word:
-				
 				rack = rack.replace(letter, '', 1)
-			
 				used_letters = used_letters + letter
-				# index = rack.find(letter)
-				# print "Letter ", letter, " was taking out of the rack"
-				# print "Current Rack:\t" ,rack
-				# temp = rack[index-1]
-				# print "takes out ", letter, " from the rack"
-				# temp2 = rack[index+1:]
-				# temp = temp + temp2
-				# rack = temp
-			# 	# print rack
-			# print "used letters\t", used_letters
-			# print " word\t", word
 				if sorted(used_letters) == sorted(word) and len(used_letters) > 1:
 					valid_words.append(word)
-
-	# This orders valid_words using the length of each word, reverse makes it Greatest to Smallest, if you leave it out it is by default a-z, smallest to largest
+	
 	for word in sorted(valid_words, key=len, reverse = True):
-		print word
+		score = 0
+		for letter in word:
+			score += scores[letter]
 
-
-
+		print word + "\t" +  str(score)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('rack',type=str)
